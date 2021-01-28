b46cc0a3bd72cbcc35ae24d8cb83777a02c3c9f2
Mujahid <mujahidmalikarain@gmail.com>
2021-01-28 06:46:42 -0300

    mujahid first commit

---
+++ Match 3 Clone/Assets/Match 3 Starter/Scripts/Board and Grid/ChangeMenu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMenu : MonoBehaviour
+{
+    public void changeMenuScene(string scenename)
+    {
+        Application.LoadLevel(scenename);
+    }
+   
+}
